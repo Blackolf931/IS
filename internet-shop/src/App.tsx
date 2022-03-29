077a83c2438489299de89c3b80a338e74aa12aed
DZIANIS KAHUKIN <dzianis.kahukin@innowise-group.com>
2022-03-29 12:05:48 +0300

    Refactoring routes

--- internet-shop/src/App.tsx
+++ internet-shop/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppContainer from './core/components/appContainer/AppContainer';
 import Main from './pages/main/Main';
+import MainRoutes from './core/constants/mainRoutes';
 
 // eslint-disable-next-line react/function-component-definition
 const App: React.FC = () => (
   <AppContainer>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path={MainRoutes.main} element={<Main />} />
+        <Route path={MainRoutes.login} element={<div>Login</div>} />
+        <Route path={MainRoutes.registration} element={<div>Registration</div>} />
       </Routes>
     </BrowserRouter>
   </AppContainer>
