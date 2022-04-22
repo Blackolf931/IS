93f828fcd2431af4ce9d86683b0f709e57ff556a
DZIANIS KAHUKIN <dzianis.kahukin@innowise-group.com>
2022-04-22 12:53:10 +0300

    Add cart shop icon to navbar

--- internet-shop/src/App.tsx
+++ internet-shop/src/App.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppContainer from './core/components/appContainer/AppContainer';
 import MainRoutes from './core/constants/mainRoutes';
 import Header from './core/components/header/Header';
 import Products from './pages/products/Products';
 
-const App: React.FC = () => (
-  <AppContainer>
-    <Header />
-    <BrowserRouter>
-      <Routes>
-        <Route path={MainRoutes.main} element={<Products />} />
-        <Route path={MainRoutes.login} element={<div>Login</div>} />
-        <Route path={MainRoutes.registration} element={<div>Registration</div>} />
-      </Routes>
-    </BrowserRouter>
-  </AppContainer>
-);
+const App: React.FC = () => {
+  const [basket, setBasket] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('basket') || '[]');
+    if (basket) {
+      setBasket(items);
+    }
+  }, []);
+
+  return (
+    <AppContainer>
+      <Header productsInBasketCount={basket.length} />
+      <BrowserRouter>
+        <Routes>
+          <Route path={MainRoutes.main} element={<Products />} />
+          <Route path={MainRoutes.login} element={<div>Login</div>} />
+          <Route path={MainRoutes.registration} element={<div>Registration</div>} />
+        </Routes>
+      </BrowserRouter>
+    </AppContainer>
+  );
+};
 export default App;
--- internet-shop/src/core/components/header/Header.tsx
+++ internet-shop/src/core/components/header/Header.tsx
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AppBar from '@mui/material/AppBar';
+import { Badge, IconButton } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchContainer from './styled/SearchContainer';
 import SearchIconWrapperContainer from './styled/SearchIconWrapperContainer';
 import InputContainer from './styled/InputContainer';
 import IconButtonContainer from './styled/IconButtonContainer';
 import TypographyContainer from './styled/TypographyContainer';
 
-const Header: React.FC = () => {
+interface Props {
+  productsInBasketCount: number;
+}
+
+const Header: React.FC<Props> = ({ productsInBasketCount }) => {
   const [isOpenFilter, setIsOpenFilter] = useState(false);
 
   const setIsOpenFilterMenu = () => {
@@ -20,15 +26,6 @@ const Header: React.FC = () => {
     }
   };
 
-  const [basket, setBasket] = useState([]);
-
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem('basket') || '[]');
-    if (basket) {
-      setBasket(items);
-    }
-  }, []);
-
   return (
     <AppBar position="static">
       <Toolbar>
@@ -37,6 +34,11 @@ const Header: React.FC = () => {
         </IconButtonContainer>
 
         <TypographyContainer>MUI</TypographyContainer>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={productsInBasketCount} color="error">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
         <SearchContainer>
           <SearchIconWrapperContainer>
             <SearchIcon />
